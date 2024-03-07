dd20efe7c03d74e07d498f30d1880bb4f05d9d45
Michael Xu <xzhseh@gmail.com>
2024-03-07 15:00:43 -0500

    feat(day3): add eval_one_step_cbv solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use app::App;
 use cond::Cond;
 use lambda::Lambda;
+use utils::{is_value, substitute_expr};
 
 pub mod app;
 pub mod cond;
@@ -41,8 +42,84 @@ impl Exp {
     /// for call-by-value in the handout, and check if your implementation
     /// accurately follows the rules
     pub fn eval_one_step_cbv(&self, exp: Exp) -> Exp {
-        match exp {
-            _ => panic!(),
+        match exp.clone() {
+            Exp::App(app) => match app.t1.clone() {
+                // -----------------------
+                // (\x. t) v -> [x := v] t
+                Exp::Lambda(lambda) => {
+                    if is_value(app.t2.clone()) {
+                        substitute_expr(lambda.arg, app.t2, lambda.exp)
+                    } else {
+                        Exp::App(Box::new(App::new(app.t1, self.eval_one_step_cbv(app.t2))))
+                    }
+                }
+                //    t1 -> t1'
+                // ---------------
+                // t1 t2 -> t1' t2
+                _ => {
+                    if is_value(app.t1.clone()) {
+                        panic!("invalid expression: {:#?}", exp);
+                    } else {
+                        Exp::App(Box::new(App::new(self.eval_one_step_cbv(app.t1), app.t2)))
+                    }
+                }
+            },
+            Exp::Cond(cond) => match cond.r#if.clone() {
+                // -----------------------------
+                // if true then t1 else t2 -> t1
+                Exp::True => cond.r#then,
+                // ------------------------------
+                // if false then t1 else t2 -> t2
+                Exp::False => cond.r#else,
+                //                    t1 -> t1'
+                // -----------------------------------------------
+                // if t1 then t2 else t3 -> if t1' then t2 else t3
+                _ => {
+                    assert!(
+                        !is_value(cond.r#if.clone()),
+                        "expect if clause not to be values except `true` or `false`"
+                    );
+                    Exp::Cond(Box::new(Cond::new(
+                        self.eval_one_step_cbv(cond.r#if),
+                        cond.r#then,
+                        cond.r#else,
+                    )))
+                }
+            },
+            Exp::IsZero(e) => match *e {
+                // ----------------    -----------------
+                // IsZero 0 -> true && IsZero _ -> false
+                Exp::Nat(num) => {
+                    if num == 0 {
+                        Exp::True
+                    } else {
+                        Exp::False
+                    }
+                }
+                //        t -> t'
+                // ---------------------
+                // IsZero t -> IsZero t'
+                _ => Exp::IsZero(Box::new(self.eval_one_step_cbv(*e))),
+            },
+            Exp::Incr(e) => match *e {
+                // ---------------
+                // Incr n -> n + 1
+                Exp::Nat(num) => Exp::Nat(num.saturating_add(1)),
+                //      t -> t'
+                // -----------------
+                // Incr t -> Incr t'
+                _ => Exp::Incr(Box::new(self.eval_one_step_cbv(*e))),
+            },
+            Exp::Decr(e) => match *e {
+                // ---------------    -----------
+                // Decr n -> n - 1 && Decr 0 -> 0
+                Exp::Nat(num) => Exp::Nat(num.saturating_sub(1)),
+                //      t -> t'
+                // -----------------
+                // Decr t -> Decr t'
+                _ => Exp::Decr(Box::new(self.eval_one_step_cbv(*e))),
+            },
+            _ => panic!("invalid expression: {:#?}", exp),
         }
     }
 
