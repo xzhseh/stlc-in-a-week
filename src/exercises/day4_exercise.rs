6057b6d45034525b87bd736864f08493cead6405
Zihao Xu <xzhseh@gmail.com>
2024-03-18 21:26:23 -0400

    feat(day4): add yCombinator related reference solution (#2)
    * add yCombinator framework
    
    * tiny refactor

--- src/exercises/day4_exercise.rs
+++ src/exercises/day4_exercise.rs
@@ -1,4 +1,12 @@
-use crate::Exp;
+use crate::{
+    expr::{
+        app::App, cond::Cond, decr::Decr, incr::Incr, is_zero::IsZero, lambda::Lambda, var::Var,
+    },
+    stlc_err::StlcError,
+    Exp, Strategy,
+};
+
+type Result<T> = std::result::Result<T, StlcError>;
 
 impl Exp {
     /// TODO(Day4-Q1): Write a function to observe different behavior when we
@@ -20,18 +28,86 @@ impl Exp {
     /// Hint: a stuck expression is something that can not be evaluated further
     /// using any of the operational rules we have defined, and is also
     /// not a *value*. (yes, `is_value` should be of help)
-    pub fn is_stuck(&self) -> bool {
+    pub fn is_stuck(&self, _strategy: Strategy) -> bool {
         todo!()
     }
 }
 
-#[allow(dead_code)]
-struct YCombinator(Exp);
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct YCombinator {
+    // The *driver* function for yCombinator
+    y: Exp,
+    // The *payload* function carried the "recursion" logic
+    // must be *lambda abstraction*
+    f: Exp,
+}
 
 impl YCombinator {
-    /// TODO(Day4-Q4): Initlialize your definition of yCombinator here
-    #[allow(dead_code)]
-    pub fn new() -> Self {
-        todo!()
+    /// TODO(Day4-Q4): Initialize the driver function of yCombinator here
+    pub fn new(f: Exp) -> Self {
+        // λF. (λx. F (x x)) (λx. F (x x))
+        let e = Lambda::build(
+            "x",
+            App::build(
+                Var::build("F"),
+                App::build(Var::build("x"), Var::build("x")),
+            ),
+        );
+        let y = Lambda::build("F", App::build(e.clone(), e));
+        Self { y, f }
+    }
+
+    /// Yes, just a simple times function, e.g., 10 * 30 == 300
+    /// this will be used as the payload function, i.e., f
+    pub fn gen_built_in_times() -> Exp {
+        Lambda::build(
+            "rec",
+            Lambda::build(
+                "x",
+                Lambda::build(
+                    "y",
+                    Lambda::build(
+                        "z",
+                        Cond::build(
+                            IsZero::build(Var::build("z")),
+                            0.into(),
+                            Cond::build(
+                                IsZero::build(Var::build("y")),
+                                App::build(
+                                    App::build(
+                                        App::build(Var::build("rec"), Var::build("x")),
+                                        Var::build("x"),
+                                    ),
+                                    Decr::build(Var::build("z")),
+                                ),
+                                Incr::build(App::build(
+                                    App::build(
+                                        App::build(Var::build("rec"), Var::build("x")),
+                                        Decr::build(Var::build("y")),
+                                    ),
+                                    Var::build("z"),
+                                )),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        )
+    }
+
+    /// TODO(Day4-Q5): Build your to-be-evaluated expression here
+    fn build_eval_expr(&self, inputs: Vec<Exp>) -> Exp {
+        let mut e = App::build(self.y.clone(), self.f.clone());
+        for input in inputs {
+            e = App::build(e, input);
+        }
+        e
+    }
+
+    /// TODO(Day4-Q6): Evaluate your packed yCombinator with the specific inputs to *normal form*
+    pub fn eval(self, inputs: Vec<Exp>, strategy: Strategy) -> Result<(Exp, u32)> {
+        let e = self.build_eval_expr(inputs);
+        let (result, steps) = e.ref_eval_to_normal_form(strategy)?;
+        Ok((result, steps))
     }
 }
